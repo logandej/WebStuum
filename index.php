f250747052997a2dd134d2de9d09ef347991718d
logandej <74404279+logandej@users.noreply.github.com>
2020-12-14 20:11:10 +0100

    de

---
+++ index.php
@@ -0,0 +1,103 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+      <meta charset="utf-8">
+      <title>Stuum- Accueil</title>
+      <link rel="stylesheet" type="text/css" href="css/styles.css"/>
+      <link rel="icon" type="image/ico" href="images/favicon.ico"/>
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+    </head>
+	<body>
+        <header id="top_page">
+            <h1>STUUM</h1>
+            <nav>
+                <div><a href="./index.html">Produits</a></div>
+                <div><a href="./contact.html">Contact</a></div>
+				<div><a href="./contact.html">Légendaire</a></div>
+                
+            </nav>
+           
+
+        </header>
+		<main>
+		
+			<h2><strong>Tee-Shirt</h2></strong>
+			<fieldset>
+				<img src="img/pieuvre.png" alt="pieuvre" id="pieuvre">
+				<img src="img/pieuvreA.png" alt="pieuvre" id="pieuvre">
+				<img src="img/pieuvreB.png" alt="pieuvre" id="pieuvre">
+				<h3 class="prix">28€</h3>
+			</fieldset>
+			
+			<fieldset>
+				<img src="img/dragon.png" alt="dragon" id="dragon">
+				<img src="img/dragonA.png" alt="dragon" id="dragon">
+				<img src="img/dragonB.png" alt="dragon" id="dragon">
+				<h3 class="prix">32€</h3>
+			</fieldset>
+			
+			<fieldset>
+				<img src="img/YYW.png" alt="YYW" id="YYW">
+				<img src="img/YYWA.png" alt="YYW" id="YYW">
+				<img src="img/YYWB.png" alt="YYW" id="YYW">
+				<h3 class="prix">30€</h3>
+			</fieldset>
+			
+			<fieldset>
+				<img src="img/YYN.png" alt="YYN" id="YYN">
+				<img src="img/YYNA.png" alt="YYN" id="YYN">
+				<img src="img/YYNB.png" alt="YYN" id="YYN">		
+				<h3 class="prix">30€</h3>
+			</fieldset>
+			<h2><strong>Sweat</h2></strong>
+			<fieldset>
+				<img src="img/JapanG.png" alt="JapanG" id="JapanG">
+				<img src="img/JapanGA.png" alt="JapanG" id="JapanG">
+				<img src="img/JapanGB.png" alt="JapanG" id="JapanG">
+				
+				<h3 class="prix">78€</h3>
+			</fieldset>
+			
+			<h2><strong>Casquette</h2></strong>
+			<article>
+			<ul>
+				<fieldset>
+				<img src="img/CasquetteN.png" alt="CasquetteN" id="CasquetteN">
+				<h3 class="prix">19€</h3>
+				</fieldset>
+			
+				<fieldset>
+				<img src="img/CasquetteW.png" alt="CasquetteW" id="CasquetteW">
+				<h3 class="prix">19€</h3>
+				</fieldset>
+			
+				<fieldset>
+				<img src="img/CasquetteB.png" alt="CasquetteB" id="CasquetteB">
+				<h3 class="prix">19€</h3>
+				</fieldset>
+			</ul>
+			</article>
+		
+		<form action="mail.php" method="post">
+            <p>
+            <input type="password" name="mot_de_passe" />
+            <input type="submit" value="Valider" />
+            </p>
+        </form>
+		</main>
+		
+		
+		<footer>
+			<div><strong> A propos </strong></div>
+			<div> Stuum est une marque de vêtements designée par Marceau DeJesus.</div>
+			<div> Nous ne sommes pas chargés de fabriquer les vêtements. Tout savoir sur <a href="./contact.html">SpreadShirt</a></div>
+			<div> Tous les droits de design sont strictements réservés et ne peuvent pas etre utilisés ©</div>
+			
+			<div><a href="https://www.instagram.com/stuum_official/?hl=fr"><img class='insta'src="img/insta.png" alt="CasquetteB" id="CasquetteB"></div>
+		
+		
+		
+		</footer>
+		
+		
+	</html>
--- mail.php
+++ mail.php
@@ -1,14 +1,31 @@
-<?php
-echo "tamerelagentille";
-$to = 'logan.dejesus1@gmail.com';
-$subject = 'Marriage Proposal';
-$message = 'Hi Jane, will you marry me?'; 
-$from = 'logan.dejesus1@gmail.com';
- 
-// Sending email
-if(mail($to, $subject, $message)){
-    echo 'Your mail has been sent successfully.';
-} else{
-    echo 'Unable to send email. Please try again.';
-}
-?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title>Codes d'accès au serveur central de la NASA</title>
+    </head>
+    <body>
+    
+        <?php
+    if (isset($_POST['mot_de_passe']) AND $_POST['mot_de_passe'] ==  "kangourou") // Si le mot de passe est bon
+    {
+    // On affiche les codes
+    ?>
+        <h1>Voici les codes d'accès :</h1>
+        <p><strong>CRD5-GTFT-CK65-JOPM-V29N-24G1-HH28-LLFV</strong></p>   
+        
+        <p>
+        Cette page est réservée au personnel de la NASA. N'oubliez pas de la visiter régulièrement car les codes d'accès sont changés toutes les semaines.<br />
+        La NASA vous remercie de votre visite.
+        </p>
+        <?php
+    }
+    else // Sinon, on affiche un message d'erreur
+    {
+        echo '<p>Mot de passe incorrect</p>';
+    }
+    ?>
+    
+        
+    </body>
+</html>
