3e3522e4b40487f69e507d3f52fae3e31a6196fa
logandej <74404279+logandej@users.noreply.github.com>
2021-01-10 09:45:01 +0100

    Update mail.php

--- mail.php
+++ mail.php
@@ -7,7 +7,7 @@
 
 <body>
     <?php
-    $retour = mail('logan.dejesus1@gmail.com', 'Envoi depuis la page Contact', $_POST['message'], 'From : webmaster@monsite.fr');
+    $retour = mail('logan.dejesus1@gmail.com', 'Envoi depuis la page Contact', $_POST['message'], 'From : https://logandej.github.io/WebStuum/');
     if ($retour) {
         echo '<p>Votre message a bien été envoyé.</p>';
     }
